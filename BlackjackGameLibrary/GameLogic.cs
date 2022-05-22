d45de3191a767f64ee48c3ac4c97b6d9428f24e9
Elvis Knapman <46550724+ElvisKnapman@users.noreply.github.com>
2022-05-22 17:56:01 -0400

    add methods and logic to game
    add CalculateHandValue method that returns the current value of the hand... add CheckIfValid method that checks to see if the value of the hand is over the upper valid bounds of 21... add CheckForBlackjack method that returns a boolean representing if the hand is exactly 21...
    
    BUG FIX: fix bug in CreateDeck Method in loop condition (change from 0 to 1). Bug allowed for cards of value 0 to be created for all 4 suits.
    
    add some logic in UI flow

--- BlackjackGameLibrary/GameLogic.cs
+++ BlackjackGameLibrary/GameLogic.cs
@@ -1,4 +1,5 @@
-﻿using BlackjackGameLibrary.Models;
+﻿using BlackjackGameLibrary.Enums;
+using BlackjackGameLibrary.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -35,14 +36,54 @@ namespace BlackjackGameLibrary
             return DrawOneCard(deck);
         }
 
-        public static int CalculateHandValue(PlayerModel player)
-        {
-            throw new NotImplementedException();
-        }
-
         public static void RequestCard(PlayerModel player)
         {
             player.playerHand.Add(DrawOneCard(BlackjackDeckModel.shuffledDeck));
         }
+
+        // Helper that will return current hand value
+        public static int CalculateHandValue(PlayerModel player)
+        {
+            int handValue = 0;
+            foreach (var card in player.playerHand)
+            {
+                if ((int)card.Value >= 10)
+                {
+                    handValue += 10;
+                } 
+                else if ((int)card.Value > 1 && (int)card.Value <= 10)
+                {
+                    handValue += (int)card.Value;
+                }
+                // if we get here, the card is an Ace (we need to determine if it's worth 1 or 11)
+                else
+                {
+                    // if current hand value is greater than 10, Ace must be used as 1
+                    if (handValue > 10)
+                    {
+                        handValue += 1;
+                    } 
+                    // if current hand value is 10 or below, use as 11
+                    else
+                    {
+                        handValue += 11;
+                    }
+                }
+            }
+            return handValue;
+        }
+
+        public static bool CheckForBlackjack(PlayerModel player)
+        {
+            return CalculateHandValue(player) == 21;
+        }
+
+        // Helper that will check if the hand is over 21 (returns boolean)
+        public static bool CheckIfHandValid(PlayerModel player)
+        {
+            return CalculateHandValue(player) <= 21;
+        }
+
+
     }
 }
